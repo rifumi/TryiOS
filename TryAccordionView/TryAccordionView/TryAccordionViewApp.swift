1c927b26e6448b2f1d2f1d1b57af867fc908f435
rifumi <89812517+rifumi@users.noreply.github.com>
2023-07-26 09:46:10 +0900

    add project TryAccordionView

---
+++ TryAccordionView/TryAccordionView/TryAccordionViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TryAccordionViewApp.swift
+//  TryAccordionView
+//
+//  Created by rifumi on 2023/07/17.
+//
+
+import SwiftUI
+
+@main
+struct TryAccordionViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
